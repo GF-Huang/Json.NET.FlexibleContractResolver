31234b9133f3dac0aa18a044a1af1fad25481781
GF-Huang <GF-Huang@users.noreply.github.com>
2020-03-20 01:20:46 +0800

    Init Project.

---
+++ Json.NET.PropertyMappingTests/Models/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Newtonsoft.Json.PropertyMappingTests.Models {
+    public class ModelContainer {
+        public Model1 Model1 { get; set; }
+
+        public Model2 Model2 { get; set; }
+    }
+
+    public class Model1 {
+        public int Property1 { get; set; }
+        public string Property2 { get; set; }
+        public bool Property3 { get; set; }
+    }
+
+    public class Model2 {
+        public int Property11 { get; set; }
+        public string Property22 { get; set; }
+        public bool Property33 { get; set; }
+    }
+}
